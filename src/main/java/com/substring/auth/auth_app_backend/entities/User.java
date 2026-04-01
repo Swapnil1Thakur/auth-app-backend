90ded05bf5978f6a1b5e2b4ab834816b40f50571
Swapnil <thakur7swapnil@gmail.com>
2026-04-01 12:54:04 +0530

    Added Spring Security configuration file and updated User entity with UserDetails implementation

--- src/main/java/com/substring/auth/auth_app_backend/entities/User.java
+++ src/main/java/com/substring/auth/auth_app_backend/entities/User.java
@@ -2,11 +2,12 @@ package com.substring.auth.auth_app_backend.entities;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.Instant;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -15,7 +16,7 @@ import java.util.UUID;
 
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(name = "user_id")
@@ -56,4 +57,33 @@ public class User {
     }
 
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;  //isko main username maanta hu, islie return kr rha hu
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
