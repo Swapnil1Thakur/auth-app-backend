300a0ea4745694c211bc888a0d11a7a23992578c
Swapnil <thakur7swapnil@gmail.com>
2026-04-02 16:50:43 +0530

    implemented JWT authentication with access token generation, refresh token generation and configuration setup

---
+++ src/main/java/com/substring/auth/auth_app_backend/security/JwtService.java
@@ -0,0 +1,87 @@
+package com.substring.auth.auth_app_backend.security;
+
+import com.substring.auth.auth_app_backend.entities.Role;
+import com.substring.auth.auth_app_backend.entities.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+@Service
+
+public class JwtService{
+    private final SecretKey key;
+    private final long accessTtlSeconds;
+    private final long refreshTtlSeconds;
+    private final String issuer;
+
+    public JwtService(@Value("${security.jwt.secret}") String secret,
+                      @Value("${security.jwt.access-ttl-seconds}") long accessTtlSeconds,
+                      @Value("${security.jwt.refresh-ttl-seconds}") long refreshTtlSeconds,
+                      @Value("${security.jwt.issuer}")String issuer) {
+
+        if(secret == null || secret.length() < 64){
+            throw new IllegalArgumentException("Invalid Secret!!");
+        }
+        this.key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+
+        this.accessTtlSeconds = accessTtlSeconds;
+        this.refreshTtlSeconds = refreshTtlSeconds;
+        this.issuer = issuer;
+    }
+
+
+    //generate token
+    public String generateToken(User user){
+        Instant now = Instant.now();
+        List<String> roles = user.getRoles() == null ? List.of() :
+                user.getRoles().stream().map(Role::getName).toList();
+
+        return Jwts.builder()
+                .id(UUID.randomUUID().toString())
+                .subject(user.getId().toString())
+                .issuer(issuer)
+                .issuedAt(Date.from(now))
+                .expiration(Date.from(now.plusSeconds(accessTtlSeconds)))
+                .claims(Map.of(
+                        "email", user.getEmail(),
+                        "roles", roles,
+                        "typ", "access"
+                ))
+                .signWith(key, SignatureAlgorithm.ES512)
+                .compact();
+    }
+
+    //generate refresh token
+    public String refreshToken(User user, String jti){
+        Instant now = Instant.now();
+        List<String> roles = user.getRoles() == null ? List.of() :
+                user.getRoles().stream().map(Role::getName).toList();
+
+        return Jwts.builder()
+                .id(UUID.randomUUID().toString())
+                .subject(user.getId().toString())
+                .issuer(issuer)
+                .issuedAt(Date.from(now))
+                .expiration(Date.from(now.plusSeconds(refreshTtlSeconds)))
+                .claim("typ", "refresh")
+                .signWith(key, SignatureAlgorithm.ES512)
+                .compact();
+    }
+
+
+
+
+    //generate refresh token
+
+}
