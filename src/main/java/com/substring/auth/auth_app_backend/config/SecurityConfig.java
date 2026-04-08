a43d3ac5e2eb07de74a0dee321b4a5371f074911
Swapnil <thakur7swapnil@gmail.com>
2026-04-08 12:58:19 +0530

    added exception using throws in authenction manager bean(to handle)

--- src/main/java/com/substring/auth/auth_app_backend/config/SecurityConfig.java
+++ src/main/java/com/substring/auth/auth_app_backend/config/SecurityConfig.java
@@ -76,7 +76,7 @@ public class SecurityConfig {
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
-        return configuration.getAuthenticationManager();
+        return configuration.getAuthenticationManager(); //added exception
     }
 
 
