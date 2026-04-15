fc6396cd52ee13c2fd38c51fcdd2dbd00653183b
Swapnil <thakur7swapnil@gmail.com>
2026-04-15 13:14:29 +0530

    temp changes before pull

--- src/main/java/com/substring/auth/auth_app_backend/dtos/TokenResponse.java
+++ src/main/java/com/substring/auth/auth_app_backend/dtos/TokenResponse.java
@@ -7,7 +7,7 @@ public record TokenResponse(
         String tokenType,
         UserDto user
 ) {
-    public static TokenResponse of(String accessToken, String refreshToken, long expiresIn, String tokenType, UserDto user){
+    public static TokenResponse of(String accessToken, String refreshToken, long expiresIn,  UserDto user){
         return  new TokenResponse(accessToken, refreshToken, expiresIn, "Bearer", user);
     }
 
